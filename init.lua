0e99666ad6a267725bdea950772102bd8f852d5d
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-12 01:00:53 +0200

    feat: use init.lua

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- set number
+-- set rnu
+-- set tabstop=4
+-- set shiftwidth=4
+-- set expandtab
+-- set ignorecase
+-- set termguicolors
+
+require("general")
--- init.vim
+++
@@ -1,8 +0,0 @@
-set number
-set rnu
-set tabstop=4
-set shiftwidth=4
-set expandtab
-set ignorecase
-set termguicolors
-
