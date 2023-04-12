5d0e19af57b9e417756941ef4ea306a739cdcfe1
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-12 21:16:40 +0200

    add nvim-cmp

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -15,4 +15,8 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup {
     'navarasu/onedark.nvim',
     'neovim/nvim-lspconfig',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'saadparwaiz1/cmp_luasnip',
+    'L3MON4D3/LuaSnip' ,
 }
