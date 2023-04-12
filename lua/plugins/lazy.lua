b1d05eac2c912c9761fdd3a56f38b6bb913b6499
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-13 01:22:13 +0200

    fix lagging lsp conf

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -23,8 +23,8 @@ require("lazy").setup {
     {'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' }},
     'mfussenegger/nvim-jdtls',
     'nvim-treesitter/nvim-treesitter',
-    'hrsh7th/cmp-buffer'
-    'hrsh7th/cmp-path'
-    'hrsh7th/cmp-cmdline'
-    'hrsh7th/nvim-cmp'
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
 }
