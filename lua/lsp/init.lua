5cde87d25f98b2656b389acde34577409d63ea9c
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-15 21:58:26 +0000

    configure on complete add paranthes

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -19,44 +19,39 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
     -- C-b (back) C-f (forward) for snippet placeholder navigation.
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true, }),
   }),
    sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       { name = 'luasnip' }, -- For luasnip users.
     }, {
       { name = 'buffer' },
+      { name = 'path' },
     }),
 }
 
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
-    { name = 'path' }
+    { name = 'path' },
   }, {
     { name = 'cmdline' }
   })
 })
 
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
 
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
@@ -68,7 +63,7 @@ for _, lsp in ipairs(servers) do
         lspconfig[lsp].setup {
             capabilities = capabilities,
         }
-    else 
+    else
         lspconfig.lua_ls.setup {
             capabilities = capabilities,
             runtime = {
