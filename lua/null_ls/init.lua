9b6b39a21476eae24de1e68dbc1937d96d66fc24
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-21 19:14:52 +0000

    feat: format on save null_ls

--- lua/null_ls/init.lua
+++ lua/null_ls/init.lua
@@ -1,12 +1,22 @@
 local null_ls = require("null-ls")
 
-null_ls.setup({
-    sources = { null_ls.builtins.formatting.prettier }
-})
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-    group = vim.api.nvim_create_augroup("Formatter", {clear = true}),
-    callback = function ()
-        vim.lsp.buf.format()
+function on_attach(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+        })
     end
+end
+
+null_ls.setup({
+    sources = { null_ls.builtins.formatting.prettier },
+    on_attach = on_attach
 })
