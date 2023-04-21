93a80fde26eb3428f28c05e6889d43874dde2c0c
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-21 18:46:35 +0000

    feat: use prettier

--- lua/null_ls/init.lua
+++ lua/null_ls/init.lua
@@ -1,10 +1,6 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.cspell,
-        null_ls.builtins.code_actions.cspell,
-
-    }
+    sources = { null_ls.builtins.formatting.prettier }
 })
 
