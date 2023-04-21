907e48f9e25898444abb75afb91dfeb5b821230d
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-21 19:11:21 +0000

    feat: add formatter on anysave

--- lua/null_ls/init.lua
+++ lua/null_ls/init.lua
@@ -4,3 +4,9 @@ null_ls.setup({
     sources = { null_ls.builtins.formatting.prettier }
 })
 
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = vim.api.nvim_create_augroup("Formatter", {clear = true}),
+    callback = function ()
+        vim.lsp.buf.format()
+    end
+})
