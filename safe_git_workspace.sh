c5afd762303905b1fe9b9fd03b3edc8d2238bc97
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-21 15:43:29 +0000

    feat: add safe directories

--- safe_git_workspace.sh
+++ safe_git_workspace.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-tmp_file="/tmp/safe_git_wokspaces_only_run_once"
-if [[ ! -f $tmp_file ]]; then
-    echo "started adding directries to safe wrokspaces"
-    for dir in $(ls /workspace); do
-        git config --global --add safe.directory "/workspace/$dir"
-    done
-    touch $tmp_file
-fi
+echo "started adding directries to safe wrokspaces"
+for dir in $(ls /workspace); do
+    git config --global --add safe.directory "/workspace/$dir"
+done
+touch $tmp_file
