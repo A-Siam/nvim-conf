aaaf51487f7982c8130899e794257d40c2cc77f6
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-22 15:13:09 +0000

    fix: use xw for workspace

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,7 +3,7 @@ require("trouble").setup {}
 vim.keymap.set('n', '<leader>xx', "<CMD>TroubleToggle<CR>", {
     silent = true, noremap = true
 })
-vim.keymap.set('n', '<leader>xx', "<CMD>TroubleToggle<CR>", {
+vim.keymap.set('n', '<leader>xw', "<CMD>TroubleToggle<CR>", {
     silent = true, noremap = true
 })
 vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", {
