80c5ce42b86f9a931f212d96e99f3330bb448cdd
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-22 21:22:40 +0000

    fix: async formatting input

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -105,7 +105,7 @@ local function on_attach(client, bufnr)
             buffer = bufnr,
             callback = function()
                 -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                async_formatting({ bufnr = bufnr })
+                async_formatting(bufnr)
             end,
         })
     end
