7e88bac45c65255fada48fb4fcfa575f2f3117b2
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-22 04:53:58 +0000

    use nvim new api for formatting

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -73,7 +73,7 @@ local function on_attach(client, bufnr)
             buffer = bufnr,
             callback = function()
                 -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                vim.lsp.buf.formatting_sync()
+                vim.lsp.buf.format({bufnr = bufnr})
             end,
         })
     end
