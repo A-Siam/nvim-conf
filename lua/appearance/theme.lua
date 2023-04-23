0181b1a59c1aa10e47cc4b6429c0138feee4f303
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-23 16:47:11 +0000

    use dark cursorline

--- lua/appearance/theme.lua
+++ lua/appearance/theme.lua
@@ -4,7 +4,7 @@ require('material').setup({
         terminal = false,            -- Enable contrast for the built-in terminal
         sidebars = true,             -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
         floating_windows = false,    -- Enable contrast for floating windows
-        cursor_line = false,         -- Enable darker background for the cursor line
+        cursor_line = true,          -- Enable darker background for the cursor line
         non_current_windows = false, -- Enable darker background for non-current windows
         filetypes = {},              -- Specify which filetypes get the contrasted (darker) background
     },
