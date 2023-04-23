7375ce82ba77dfc6397d1c22044c7aa3310b2098
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-23 15:19:44 +0000

    feat: add non active color dim

--- lua/appearance/theme.lua
+++ lua/appearance/theme.lua
@@ -1,21 +1,21 @@
 -- theme setup
 require('material').setup({
     contrast = {
-        terminal = false,            -- Enable contrast for the built-in terminal
-        sidebars = true,             -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-        floating_windows = false,    -- Enable contrast for floating windows
-        cursor_line = false,         -- Enable darker background for the cursor line
-        non_current_windows = false, -- Enable darker background for non-current windows
-        filetypes = {},              -- Specify which filetypes get the contrasted (darker) background
+        terminal = false,           -- Enable contrast for the built-in terminal
+        sidebars = true,            -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+        floating_windows = false,   -- Enable contrast for floating windows
+        cursor_line = false,        -- Enable darker background for the cursor line
+        non_current_windows = true, -- Enable darker background for non-current windows
+        filetypes = {},             -- Specify which filetypes get the contrasted (darker) background
     },
-    plugins = {                      -- Uncomment the plugins that you use to highlight them
+    plugins = {                     -- Uncomment the plugins that you use to highlight them
         "nvim-tree",
         "nvim-web-devicons",
         "trouble",
     },
     disable = {
         colored_cursor = true, -- Disable the colored cursor
-         borders = true,
+        borders = true,
     },
     high_visibility = {
         lighter = false, -- Enable higher contrast text for lighter style
