f2cfb434208fa687c743d33d2a8ba2028c605993
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-25 23:45:44 +0000

    feat: add save all before lazygit

--- lua/general.lua
+++ lua/general.lua
@@ -1,3 +1,4 @@
+local vim = vim
 local opt = vim.opt
 local wo = vim.wo
 local go = vim.go
@@ -12,15 +13,18 @@ wo.rnu = true
 go.ignorecase = true
 
 -- general keymaps
-vim.keymap.set('n', '<leader>tt', ":tabnew<CR>:term ".. os.getenv("MAIN_SHELL") .. "<CR>a", {})
-vim.keymap.set('n', '<leader>gg', ":LazyGit<CR>", {})
+vim.keymap.set('n', '<leader>tt', ":tabnew<CR>:term " .. os.getenv("MAIN_SHELL") .. "<CR>a", {})
+vim.keymap.set('n', '<leader>gg', function()
+    vim.cmd [[ wa ]]
+    vim.cmd [[ LazyGit ]]
+end, {})
 vim.keymap.set('n', '<leader>nh', ":noh<CR>", {})
 vim.keymap.set('i', '<c-s>', "<cmd>lua vim.lsp.buf.signature_help()<CR>", {})
 vim.cmd [[
-    imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+    imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
     " -1 for jumping backwards.
     inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
-    
+
     snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
     snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
 ]]
