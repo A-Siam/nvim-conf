352d567728a7a74fb6093f6c3e8fa981ea0ef28b
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-25 15:34:49 +0000

    feat: add golang for null ls

--- lua/null_ls/init.lua
+++ lua/null_ls/init.lua
@@ -1,5 +1,8 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = { null_ls.builtins.formatting.prettier },
+    sources = {
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.gofmt
+    },
 })
