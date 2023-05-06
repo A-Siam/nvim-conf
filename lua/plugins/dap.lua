e62816444de9c3a7ababedbd9f0395e240579fb4
AbdullahKhaled-Fillflix <abdullah.khaled@eg.fillflix.com>
2023-05-06 01:23:46 +0000

    feat: format dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,46 +2,45 @@ local dap = require('dap')
 
 -- python
 dap.adapters.python = {
-  type = 'executable';
-  command = 'python3';
-  args = { '-m', 'debugpy.adapter' };
+    type = 'executable',
+    command = 'python3',
+    args = { '-m', 'debugpy.adapter' },
 }
 
 dap.configurations.python = {
-  {
-    -- The first three options are required by nvim-dap
-    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
-    request = 'launch';
-    name = "Launch file";
+    {
+        -- The first three options are required by nvim-dap
+        type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+        request = 'launch',
+        name = "Launch file",
+        -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
-    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
-    program = "${file}"; -- This configuration will launch the current file if used.
-    pythonPath = function()
-      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-      local cwd = vim.fn.getcwd()
-      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-        return cwd .. '/venv/bin/python'
-      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-        return cwd .. '/.venv/bin/python'
-      else
-        return '/usr/bin/python3'
-      end
-    end;
-  },
+        program = "${file}", -- This configuration will launch the current file if used.
+        pythonPath = function()
+            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+            local cwd = vim.fn.getcwd()
+            if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+                return cwd .. '/venv/bin/python'
+            elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+                return cwd .. '/.venv/bin/python'
+            else
+                return '/usr/bin/python3'
+            end
+        end,
+    },
 }
 
 -- nodejs
 require("dap-vscode-js").setup({
-  node_path = "ts-node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-  debugger_path = "/root/.local/share/nvim/lazy/vscode-js-debug", -- Path to vscode-js-debug installation.
-  -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
-  -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-  -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-  -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+    node_path = "ts-node",                                                                     -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+    debugger_path = "/root/.local/share/nvim/lazy/vscode-js-debug",                            -- Path to vscode-js-debug installation.
+    -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+    adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+    -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+    -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+    -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
 })
 
 for _, language in ipairs({ "typescript", "javascript" }) do
@@ -57,17 +56,17 @@ for _, language in ipairs({ "typescript", "javascript" }) do
             type = "pwa-node",
             request = "attach",
             name = "Attach",
-            processId = require'dap.utils'.pick_process,
+            processId = require 'dap.utils'.pick_process,
             cwd = "${workspaceFolder}",
         },
         {
             type = "pwa-node",
             request = "launch",
             name = "Launch via npm",
-            processId = require'dap.utils'.pick_process,
+            processId = require 'dap.utils'.pick_process,
             cwd = "${workspaceFolder}",
-            runtimeExecutable= "npm",
-            runtimeArgs = {"run-script", "debug"}
+            runtimeExecutable = "npm",
+            runtimeArgs = { "run-script", "debug" }
         },
         {
             type = "pwa-node",
@@ -89,37 +88,37 @@ end
 
 -- golang
 dap.adapters.delve = {
-  type = 'server',
-  port = '${port}',
-  executable = {
-    command = 'dlv',
-    args = {'dap', '-l', '127.0.0.1:${port}'},
-  }
+    type = 'server',
+    port = '${port}',
+    executable = {
+        command = 'dlv',
+        args = { 'dap', '-l', '127.0.0.1:${port}' },
+    }
 }
 
 -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
 dap.configurations.go = {
-  {
-    type = "delve",
-    name = "Debug",
-    request = "launch",
-    program = "${file}"
-  },
-  {
-    type = "delve",
-    name = "Debug test", -- configuration for debugging test files
-    request = "launch",
-    mode = "test",
-    program = "${file}"
-  },
-  -- works with go.mod packages and sub packages 
-  {
-    type = "delve",
-    name = "Debug test (go.mod)",
-    request = "launch",
-    mode = "test",
-    program = "./${relativeFileDirname}"
-  }
+    {
+        type = "delve",
+        name = "Debug",
+        request = "launch",
+        program = "${file}"
+    },
+    {
+        type = "delve",
+        name = "Debug test", -- configuration for debugging test files
+        request = "launch",
+        mode = "test",
+        program = "${file}"
+    },
+    -- works with go.mod packages and sub packages
+    {
+        type = "delve",
+        name = "Debug test (go.mod)",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}"
+    }
 }
 
 -- dapui
@@ -127,18 +126,16 @@ local dapui = require("dapui")
 dapui.setup()
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+    dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+    dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+    dapui.close()
 end
 
 -- keymaps
-vim.keymap.set('n', '<space>dd', dapui.toggle , {})
-vim.keymap.set('n', '<space>df', "<CMD>lua require'dap'.toggle_breakpoint()<CR>" , {})
+vim.keymap.set('n', '<space>dd', dapui.toggle, {})
+vim.keymap.set('n', '<space>df', "<CMD>lua require'dap'.toggle_breakpoint()<CR>", {})
 vim.keymap.set('n', '<space>dfc', ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", {})
-
-
