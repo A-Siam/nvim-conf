04f0b000fb8f57576c3cccb0b8ab52f554aa8ed1
A-Siam <abdullah.khaled.kamal0@gmail.com>
2023-05-31 17:55:42 +0000

    feat: add git config message

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #/usr/bin/env bash
+if [[ ! -f ~/.gitconfig ]]; then
+    echo "please configure git first"
+    exit 1
+fi
 cp ~/.gitconfig .
 docker build -t docker_nvim:latest \
     --build-arg CACHEBUST=$(date +%s)  .      
