12a1a1b4785358cc5d63c42b3c7ea13df3e8a190
A-Siam <abdullah.khaled.kamal0@gmail.com>
2023-05-31 01:16:54 +0000

    feat: set nord theme

--- lua/appearance/theme.lua
+++ lua/appearance/theme.lua
@@ -1,28 +1,28 @@
 -- theme setup
 -- Material theme setup
-require('material').setup({
-    contrast = {
-        terminal = false,            -- Enable contrast for the built-in terminal
-        sidebars = true,             -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-        floating_windows = false,    -- Enable contrast for floating windows
-        cursor_line = true,          -- Enable darker background for the cursor line
-        non_current_windows = false, -- Enable darker background for non-current windows
-        filetypes = {},              -- Specify which filetypes get the contrasted (darker) background
-    },
-    plugins = {                      -- Uncomment the plugins that you use to highlight them
-        "nvim-tree",
-        "nvim-web-devicons",
-        "trouble",
-        "indent-blankline",
-    },
-    disable = {
-        colored_cursor = true, -- Disable the colored cursor
-        borders = false,
-    },
-    high_visibility = {
-        lighter = false, -- Enable higher contrast text for lighter style
-        darker = false   -- Enable higher contrast text for darker style
-    },
-})
+-- require('material').setup({
+--     contrast = {
+--         terminal = false,            -- Enable contrast for the built-in terminal
+--         sidebars = true,             -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+--         floating_windows = false,    -- Enable contrast for floating windows
+--         cursor_line = true,          -- Enable darker background for the cursor line
+--         non_current_windows = false, -- Enable darker background for non-current windows
+--         filetypes = {},              -- Specify which filetypes get the contrasted (darker) background
+--     },
+--     plugins = {                      -- Uncomment the plugins that you use to highlight them
+--         "nvim-tree",
+--         "nvim-web-devicons",
+--         "trouble",
+--         "indent-blankline",
+--     },
+--     disable = {
+--         colored_cursor = true, -- Disable the colored cursor
+--         borders = false,
+--     },
+--     high_visibility = {
+--         lighter = false, -- Enable higher contrast text for lighter style
+--         darker = false   -- Enable higher contrast text for darker style
+--     },
+-- })
 
-vim.cmd [[ colorscheme material ]]
+vim.cmd [[ colorscheme nord ]]
